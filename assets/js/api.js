248e8adea14ef86ce98c021eee23c97d527f9a28
mohamed0690 <mohamedmahrous069@gmail.com>
2023-08-12 03:08:51 +0300

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,24 @@
+"use strict";
+const API_KEY = "1d7ed538539a20555350a2d195db97ba";
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
